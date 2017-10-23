8e4ba4b93e034a60e04a6600b56354e00613419b
willikay11 <willikay11@gmail.com>
2017-10-23 12:53:29 +0300

    Implemented designs for login

--- resources/views/login/confirm.blade.php
+++ resources/views/login/confirm.blade.php
@@ -2,15 +2,49 @@
 
 @section('content')
 
-    <div class="col-lg-offset-1 col-lg-6">
-        {!! Form::open(['route' => ['login.confirm', $email]]) !!}
+    <div class="login-background">
+        <div class="row">
+            <div class="login-container">
+                <div class="col-lg-6 inner-left">
+                    <img class="login-image" src="img/EBG-logo.png">
+                </div>
+                <div class="col-lg-6 inner-right">
 
-        {!! Form::text('one_time_key', null) !!}
+                    <div class="ebg-input-container">
+                        @include('layouts.partials.errors')
 
-        {!! Form::submit('Login') !!}
+                        @include('layouts.partials.flash')
+                    </div>
 
-        {!! Form::close() !!}
+                    <div class="col-lg-12 login-header">
+                        <h3>Token</h3>
+                        <p>Enter the six digit token you have received via your email. </p>
+                    </div>
+
+                    {!! Form::open(['route' => ['login.confirm', $email]]) !!}
+                    <div class="row">
+                        <div class="col-lg-12 form-group ebg-input-container">
+                            {!! Form::text('one_time_key', null, ['class' => 'form-control ebg-input']) !!}
+                        </div>
+
+                        <div class="col-lg-12 ebg-input-container">
+                            <div class="col-lg-6">
+                                {!! Form::submit('Continue', ['class' => 'btn ebg-button']) !!}
+                            </div>
+                        </div>
+                    </div>
+
+                    {!! Form::close() !!}
+
+                    <div class="login-header ebg-input-container">
+                        <p>To use this system you must be an employee of EGB</p>
+                        <p>If you do not have an account, kindly request one from the administrator </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
     </div>
-
 
 @stop
