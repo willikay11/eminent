690f1eb8dfe64941a7e83bfde7463769730e3e5c
willikay11 <willikay11@gmail.com>
2017-12-15 16:44:48 +0300

    Added a 404 page

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,10 +4,12 @@ namespace App\Exceptions;
 
 use eminent\Exceptions\AuthorizationFailedException;
 use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Support\Facades\Auth;
 use Rollbar\Rollbar;
 use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -40,37 +42,37 @@ class Handler extends ExceptionHandler
      */
     public function report(Exception $exception)
     {
-//        if(getenv('APP_ENV') !== 'local' and $this->shouldReport($exception))
-//        {
-//            if(Auth::user())
-//            {
-//                $user = [
-//                    'id' => Auth::id(),
-//                    'email' => Auth::user()->email
-//                ];
-//            }
-//            else
-//            {
-//                $user = [
-//                    'id' => 'guest',
-//                    'email' => 'guest'
-//                ];
-//            }
-//
-//            $config = array(
-//                // required
-//                'access_token' => getenv('ROLLBAR_TOKEN'),
-//                // optional - environment name. any string will do.
-//                'environment' => getenv('APP_ENV'),
-//                // optional - path to directory your code is in. used for linking stack traces.
-//                'root' => base_path(),
-//
-//                'person' => $user
-//            );
-//
-//            Rollbar::init($config);
-//            Rollbar::report_exception($exception);
-//        }
+        if(getenv('APP_ENV') !== 'local' and $this->shouldReport($exception))
+        {
+            if(Auth::user())
+            {
+                $user = [
+                    'id' => Auth::id(),
+                    'email' => Auth::user()->email
+                ];
+            }
+            else
+            {
+                $user = [
+                    'id' => 'guest',
+                    'email' => 'guest'
+                ];
+            }
+
+            $config = array(
+                // required
+                'access_token' => getenv('ROLLBAR_TOKEN'),
+                // optional - environment name. any string will do.
+                'environment' => getenv('APP_ENV'),
+                // optional - path to directory your code is in. used for linking stack traces.
+                'root' => base_path(),
+
+                'person' => $user
+            );
+
+            Rollbar::init($config);
+            Rollbar::report_exception($exception);
+        }
 
         parent::report($exception);
     }
@@ -92,6 +94,16 @@ class Handler extends ExceptionHandler
 
                 break;
             }
+
+            case($exception instanceof ModelNotFoundException):
+            {
+                $e = new NotFoundHttpException($exception->getMessage(), $exception);
+
+                return response()->view('errors.404', [], 404);
+
+                break;
+            }
+
         }
 
         return parent::render($request, $exception);
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.default')
+
+@section('content')
+
+    <div class="login-background">
+        <div class="row">
+            <div class="login-container">
+                <div class="login-overlay"></div>
+                <div class="col-lg-offset-1 col-lg-10 inner-left">
+                    <div class="inner-left-overlay"></div>
+                    <div class="vertical-center">
+                        <img class="login-image" src="/img/EBG-logo.png">
+                        <div class="login-text">
+                            <p class="login-welcome-text">404. Page not found</p>
+                            <p class="login-banner">Page that you requested could not be found. Please visit this link to continue
+                                <a style="color: black" href="/">Homepage</a></p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
