0476ffde9bc91a29f13ed1cdbf9b6f0e357b1f01
willikay11 <willikay11@gmail.com>
2018-04-05 12:49:22 +0300

    Modified departments

--- resources/views/departments/index.blade.php
+++ resources/views/departments/index.blade.php
@@ -1,23 +1,42 @@
 @extends('dashboard.default')
 
 
-@section('dashboard-content')
+@section('main-content')
 
-    <div class="row">
-        <div class="col-lg-12">
-            <departments-table inline-template>
-                <div class="panel panel-default">
-                    <div class="col-lg-12 panel-header">
-                        <div class="col-lg-6">
-                            <h4>Departments</h4>
+    <section class="content-header">
+        <h1>
+            Department List
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="/"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li class="active">Department</li>
+        </ol>
+    </section>
+
+
+    <section class="content">
+        <departments-table inline-template>
+            <div class="row">
+                <!-- left column -->
+                <div class="col-md-12">
+                    <!-- general form elements -->
+                    <div class="box box-primary">
+                        <div class="box-header with-border">
+                            <div class="pull-left">
+                                <h3 class="box-title" style="padding-bottom: 25px">Departments</h3>
+                            </div>
+
+                            <div class="pull-right box-tools">
+                                <div class="col-lg-6" style="text-align: right">
+                                    @if(in_array(20, getPermissions()))
+                                        <el-button type="primary" plain icon="el-icon-plus"
+                                                   v-on:click="showAddDialog()">Add Department
+                                        </el-button>
+                                    @endif
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-lg-6" style="text-align: right">
-                            @if(in_array(20, getPermissions()))
-                                <button class="btn ebg-button" v-on:click="showAddDialog()">Add Department</button>
-                            @endif
-                        </div>
-                    </div>
-                    <div class="panel-body">
+
                         <el-table
                                 :data="tableData"
                                 stripe
@@ -45,45 +64,47 @@
                                 </el-table-column>
                             @endif
                         </el-table>
-                    </div>
-                    <hr class="panel-hr">
-                    <div class="panel-footer">
-                        <div class="block">
-                            <el-pagination
-                                    layout="prev, pager, next"
-                                    :total="total">
-                            </el-pagination>
+
+                        <div class="panel-footer">
+                            <div class="block">
+                                <el-pagination
+                                        layout="prev, pager, next"
+                                @current-change="handleCurrentChange"
+                                :total="total">
+                                </el-pagination>
+                            </div>
                         </div>
+
+                        <el-dialog
+                                title="New/Edit Profession"
+                                :visible.sync="dialogVisible"
+                                size="tiny">
+                            <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-position="top">
+
+                                <el-form-item prop="name" label="Department Name">
+                                    <el-input placeholder="Input Name" v-model="ruleForm.name"></el-input>
+                                </el-form-item>
+
+                                <el-form-item prop="active" label="Active">
+                                    <el-select v-model="ruleForm.active" placeholder="Select">
+                                        <el-option
+                                                v-for="item in options"
+                                                :key="item.value"
+                                                :label="item.label"
+                                                :value="item.value">
+                                        </el-option>
+                                    </el-select>
+                                </el-form-item>
+                            </el-form>
+                            <span slot="footer" class="dialog-footer">
+                                <el-button @click="dialogVisible = false">Cancel</el-button>
+                                <el-button type="primary" @click="add('ruleForm')">Save</el-button>
+                                </span>
+                        </el-dialog>
+
                     </div>
-
-                    <el-dialog
-                            title="New/Edit Profession"
-                            :visible.sync="dialogVisible"
-                            size="tiny">
-                        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-position="top">
-
-                            <el-form-item prop="name" label="Department Name">
-                                <el-input placeholder="Input Name" v-model="ruleForm.name"></el-input>
-                            </el-form-item>
-
-                            <el-form-item prop="active" label="Active">
-                                <el-select v-model="ruleForm.active" placeholder="Select">
-                                    <el-option
-                                            v-for="item in options"
-                                            :key="item.value"
-                                            :label="item.label"
-                                            :value="item.value">
-                                    </el-option>
-                                </el-select>
-                            </el-form-item>
-                        </el-form>
-                        <span slot="footer" class="dialog-footer">
-                <el-button @click="dialogVisible = false">Cancel</el-button>
-                            <el-button type="primary" @click="add('ruleForm')">Save</el-button>
-            </span>
-                    </el-dialog>
                 </div>
-            </departments-table>
-        </div>
-    </div>
+            </div>
+        </departments-table>
+    </section>
 @stop
